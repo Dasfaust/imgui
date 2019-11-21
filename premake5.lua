7cd9dbf8d266bd8bb7b056a199a75bed4d243754
Dasfaust <Dasfaust@users.noreply.github.com>
2019-11-21 05:59:37 -0500

    Add premake

---
+++ premake5.lua
@@ -0,0 +1,23 @@
+project "imgui"
+    kind "StaticLib"
+    language "C++"
+    systemversion "latest"
+    cppdialect "C++17"
+    staticruntime "on"
+    
+	  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("build/" .. outputdir .. "/%{prj.name}")
+
+	  files
+	  {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
